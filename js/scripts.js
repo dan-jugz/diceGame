958dd82cec8f3deda0c8453462d3b3d9033aa6d4
dan jugz <njugunadaniel364@gmail.com>
2019-04-07 06:35:53 +0300

    players rolls format input in js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function rollDie(){
+  var die1 = document.getElementById('die1')
+  var die2 = document.getElementById('die2')
+  var status = document.getElementById('status')
+  var d1 = Math.floor(Math.random()*6)+1;
+  var d2 = Math.floor(Math.random()*6)+1;
+  var diceTotal = d1 + d2 ;
+
+}
