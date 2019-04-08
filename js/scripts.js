f481ae2eb05939594b9fc81a5d0cdf60f906ba3d
dan jugz <njugunadaniel364@gmail.com>
2019-04-08 04:13:19 +0300

    completing the game logic

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,8 @@ function Player(turn) {
   this.turn = turn;
   this.playerName;
 }
+
+//player1
 Player.prototype.rollone = function() {
   if (this.roll === 1) {
   this.tempscore = 0;
@@ -22,11 +24,12 @@ Player.prototype.rollone = function() {
   this.tempscore += this.roll;
   }
 }
+
 // hold
 Player.prototype.hold = function () {
   this.totalscore += this.tempscore;
   this.tempscore = 0;
-  // this.changeturn();
+  this.changeTurn();
   alert(this.playerName + ", your turn is over, pass the mouse!");
 }
 
@@ -35,4 +38,12 @@ Player.prototype.winnerCheck = function () {
   if (this.totalscore >= 100) {
     alert(this.playerName + " You are the winner!");
   }
+}
+
+player.prototype.newGame = function () {
+  //debugger;
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.playerName ="";
 }
