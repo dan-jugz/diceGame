4ace39140880a525d25f065064b8440389339f49
dan jugz <njugunadaniel364@gmail.com>
2019-04-08 04:22:40 +0300

    user interface initiation

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,13 @@ var clearValues = function(){
   $(".player1Name").val("");
   $(".player2Name").val("");
 }
+
+// User Interface
+$(document).ready(function() {
+
+  $("button#start").click(function(event){
+    player1 = new Player(true);
+    player2 =  new Player(false);
+    $(".player-console").show();
+    $(".start-menu").hide();
+});    
